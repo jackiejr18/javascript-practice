b8281e9d9b59f777c3fac9d974d2fbd5ae556788
Jackie Jr <jwysinger@gmail.com>
2023-11-23 20:54:25 -0600

    explains global scope

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,7 +1,14 @@
-/* The const keyword in javascript is used to declare a constant variable. This means that the variable cannot
-be reassigned to a new value. */ 
+/* Scope in javascript refers to the visibility of variables and functions within a program. Threre are three types
+of scope: global scope, function scope, and block scope.  */ 
 
-const a = 4; 
-console.log(a);
 
-a = 5; 
+/* Global scope is the outermost scope. Variables and functions declared in the global scope are visible from
+anywhere in the program. */
+
+var x = 'hello, GreatStack';
+
+function example() {
+  console.log(x); 
+}
+
+example(); 
