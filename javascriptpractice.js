18873c950e47f5619ae5f58cae3e849b2c40aa50
Jackie Jr <jwysinger@gmail.com>
2023-11-24 19:17:57 -0600

    comparison operator examples logged

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,21 +1,15 @@
 // Comparison operators
 
-// Comparison operators compare two values and give back a boolean value: either true or false. 
+let a = 10;
+let b = 20;
 
-// Comparison operators are useful in decision making and loop programs in javaScript. 
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
 
-/*
+console.log(a == b);
+console.log(a != b);
 
-< (less than)
-> (greater than)
-<= (less than or equal to)
->= (greater than or equal to)
-
-==(equal checks)
-!=(inequality) (not equal) (flipped valueof equal checks)
-
-=== (strict equality checks) (checks the data type)
-
-!== (strict inequality) (!==) (flipped value strict equality check) 
-
-*/ 
+console.log(a === b);
+console.log(a !== b);
