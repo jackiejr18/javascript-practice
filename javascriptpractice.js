5574a4d10c11cc7eb2adbb39cafe2264937a6fb9
Jackie Jr <jwysinger@gmail.com>
2023-11-26 10:28:13 -0600

    control flow statements explanation

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,15 +1,3 @@
-// Operator Associativity
-
-// Operator associativity in javascript defines the order in which operators of the same precedence are evaluated
-
-// right-to-left
-
-// In right-to-left associativity operators are evaluated from right to left. 
-
-let result = 2 ** 3 ** 2; 
-
-console.log(result);
-
-// 3 ** 2 = 9
-// 2 ** 9 = 512
-
+// Control flow statements are used to control the flow of execution in a javascript program
+// they are used to make decisions, execute loops and handle errors. 
+//there are three types of control flow statements: conditional statements, loops, and try/catch statements
