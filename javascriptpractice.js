4aece5cb1b87c9fcfce16b95b5028b2876a0403f
Jackie Jr <jwysinger@gmail.com>
2023-11-26 17:03:51 -0600

    while loop syntax

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -2,15 +2,13 @@
 
 // if you want to display a message 100 times, then you can use a loop
 
-//for lop 
+//while loop
 
-for (let i = 0; i <= 4; i++) {
-  console.log(i);
-
-  for (let j = 1; j <= 3; j++) {
-    console.log('Inner loop' + j);
-  }
+while (condition) {
+  // code to be repeated
 }
+
+
 
 
 
