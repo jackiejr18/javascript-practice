887566dabacdc7f9ff13c50fe18de9c6b15fb010
Jackie Jr <jwysinger@gmail.com>
2023-11-26 11:06:00 -0600

    another example of if statement

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,8 +1,20 @@
 // Conditional Statements
 
 
-let age = 20;
+/*
+
+let age = 16;
 
 if(age > 18) {
   console.log('You are an adult.');
+}
+
+*/
+
+let country = 'India'; 
+
+let age = 20; 
+
+if(age >= 18 && country == 'India') {
+  console.log("You can get a driver's license.");
 }
