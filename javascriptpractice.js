d17eb6c89ba946a81a99373a977093976138964e
Jackie Jr <jwysinger@gmail.com>
2023-11-26 11:11:04 -0600

    if else statement

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,20 +1,12 @@
 // Conditional Statements
 
-
-/*
+// If else
 
 let age = 16;
 
 if(age > 18) {
-  console.log('You are an adult.');
+  console.log('You are an adult.')
 }
-
-*/
-
-let country = 'India'; 
-
-let age = 20; 
-
-if(age >= 18 && country == 'India') {
-  console.log("You can get a driver's license.");
+else {
+  console.log('You are a minor.');
 }
