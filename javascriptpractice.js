4a41cbadbbe0e8068687a79aa9035947157a4690
Jackie Jr <jwysinger@gmail.com>
2023-11-26 10:33:19 -0600

    types of conditional statements

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,3 +1,7 @@
-// Control flow statements are used to control the flow of execution in a javascript program
-// they are used to make decisions, execute loops and handle errors. 
-//there are three types of control flow statements: conditional statements, loops, and try/catch statements
+// Conditional Statements
+
+//conditional statements are used to execute different actions based on different conditions
+
+// if statement,
+// else statement
+// else if statement 
