b83079e35f5d3f5b58e17915b6cac386e5bc27b5
Jackie Jr <jwysinger@gmail.com>
2023-11-26 12:05:56 -0600

    ternary operator example

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,44 +1,20 @@
 // Conditional Statements
 
-// switch
+//Ternary operator
 
-/* the switch statement in javascript is a conditional statement that is used to execute a block of code based on 
-the value of expression. */ 
-
-let dayName = 5;
-
-switch (dayName) {
-  case 1:
-    dayName = 'Sunday';
-    break;
-
-  case 2:
-    dayName = 'Monday';
-    break;
-
-  case 3:
-    dayName = 'Tuesday';
-    break;
-
-  case 4:
-    dayName = 'Wednesday';
-    break;
-
-  case 5:
-    dayName = 'Thursday';
-    break;
-
-  case 6:
-      dayName = 'Friday';
-      break;
-
-  case 7:
-    dayName = 'Saturday';
-    break;
-
-  default:
-    dayName = 'Invalid Day Number'
-    break;
+/* let age = 20;
+if(age >= 18) {
+  console.log('You are an adult.');
 }
+else {
+  console.log('You are a minor.')
+} 
 
-console.log('The day is: ' + dayName);
+*/ 
+
+// condition ? value_if_true : value_if_false
+
+let age = 20;
+let msg = (age >= 18) ? 'You are an adult' : 'You are a minor.'; 
+
+console.log(msg);
