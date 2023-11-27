b0ec6fd6992c1162d913b8abcbb964eedd47c092
Jackie Jr <jwysinger@gmail.com>
2023-11-27 15:49:05 -0600

    anonymous function example2

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -2,9 +2,8 @@
 
 //Anoymous functions in JavaScript, are functions that are not declared with a name. 
 
-
-let sum = function(x, y) {
-  return x + y; 
+(
+function() {
+  console.log('Welcome to GreatStack');
 }
-
-console.log(sum(10, 15)); 
+)(); 
