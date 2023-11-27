c354a278ab847329a2bd7d9ab99a67abfa3c6c2f
Jackie Jr <jwysinger@gmail.com>
2023-11-27 11:30:41 -0600

    return a function inside of a function

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -4,11 +4,13 @@
 
 // The return statement denotes that the function has ended. Any code after return is not executed. 
 
-function add(a, b) {
-  return a + b;
-  a*b; // line wont run
+function fn1(x) {
+  function fn2(y) {
+    return x * y; 
+  }
+  return fn2; 
 }
 
-let result = add(10,40); 
+let result = fn1(3); 
 
-console.log('The sum is: ' + result);
+console.log(result); 
