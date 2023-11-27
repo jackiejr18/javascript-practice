fd74e1d457d5de1f5e0c95cb730641adc1e17ac5
Jackie Jr <jwysinger@gmail.com>
2023-11-27 09:08:21 -0600

    function example with parameters and called arguments

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,9 +1,14 @@
 // A function is a block of code that performs a specific task
 //Functions in JavaScript are reusable blocks of code that can be called from anywhere in your program. 
 
+// Parameters and Arguements
 
-function greet() {
-  console.log('Hello, GreatStack');
+// Parameters are the variables that are declared in the function definition
+
+// Arguments are the values that are passed to the function when it is called.
+
+function greet(firstName, lastName) {
+  console.log('Hello ' + firstName + ' ' + lastName);
 }
 
-greet(); 
+greet('Elon', 'Musk');
