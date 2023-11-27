4bcad8ff3854462855cbe242ac380504a4e202d7
Jackie Jr <jwysinger@gmail.com>
2023-11-27 10:59:38 -0600

    default parameters exanple3

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -5,7 +5,7 @@
 // This means that, if the argument is not passed to the function, the default value will be used. 
 
 function sum(x, y = 0) {
-  console.log(x + y );
+  console.log(x + y ); // 50 + 0
 }
 
-sum(10);
+sum(50);
