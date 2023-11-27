0e2330c0afc2dfece4c31607fea13d5e4dbe1cc0
Jackie Jr <jwysinger@gmail.com>
2023-11-27 08:40:09 -0600

    function syntax example

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,19 +1,7 @@
-// In programming, loops are used to repeat a block of code
-
-// if you want to display a message 100 times, then you can use a loop
-
-//break / continue
-
-//the "break" statement is used to terminate the loop immediately
-/* the "continue" statement is used to skip the current iteration of the loop and the control flow of the program
-goes to the next iteration */
+// A function is a block of code that performs a specific task
+//Functions in JavaScript are reusable blocks of code that can be called from anywhere in your program. 
 
 
-
-for (let i = 1; i <= 5; i++) {
-
-  if (i == 3) {
-    continue;
-  }
-  console.log(i);
+function name(params) {
+  //code to be executed
 }
