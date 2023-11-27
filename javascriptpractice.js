d3af7c2ed35cd1dbd72c740178273c1035484c06
Jackie Jr <jwysinger@gmail.com>
2023-11-27 15:53:40 -0600

    anonymous function example3 with two second wait

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -2,8 +2,6 @@
 
 //Anoymous functions in JavaScript, are functions that are not declared with a name. 
 
-(
-function() {
-  console.log('Welcome to GreatStack');
-}
-)(); 
+setTimeout(function(){
+  console.log('Hello, GreatStack');
+}, 2000);
