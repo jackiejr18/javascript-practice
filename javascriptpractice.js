9ffcd8ece2a16d8e913eba83671878a88e6d7bc8
Jackie Jr <jwysinger@gmail.com>
2023-11-27 19:56:02 -0600

    object example1 with notes

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,6 +1,7 @@
 //Objects in javaScript
 
-// Javascript object is a non-primitive data-type that allows you to store multiple collections of data. 
+// Javascript object is a non-primitive data-type that allows you to store multiple collections of data.
+// "key: value" pairs are called properties, key is always stored as string
 
 const person = {
   firstName: 'Elon',
