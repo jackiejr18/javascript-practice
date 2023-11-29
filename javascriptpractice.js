2ca190186233a3d12f360115de9c001d54b5cc0b
Jackie Jr <jwysinger@gmail.com>
2023-11-29 11:26:24 -0600

    constructor function example with two logs of different object

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -8,13 +8,13 @@ of your constructor function. */
 
 // We can use "new" operator to create an Object from a constructor function. 
 
-function Person() {
-  this.firstName = 'Elon',
-  this.lastName = 'Musk'
+function Person(first, last) {
+  this.firstName = first,
+  this.lastName = last
 }
 
-const person1 = new Person(); 
-const person2 = new Person();
+const person1 = new Person('Elon', 'Musk'); 
+const person2 = new Person('Bill', 'Gates');
 
 console.log(person1);
 console.log(person2); 
