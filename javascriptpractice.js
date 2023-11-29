41bcc7c2dca9e1802d739bba1616234d16f33024
Jackie Jr <jwysinger@gmail.com>
2023-11-28 19:20:04 -0600

    called an object method

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,15 +1,12 @@
-//Objects in javaScript
+// JavaScript Object Methods
 
-// Javascript object is a non-primitive data-type that allows you to store multiple collections of data.
-// "key: value" pairs are called properties, key is always stored as string
-// when we declare a function as a value in key:value pair then it is known as Methods
+// JavaScript method is an object property that contains a function definition. 
 
-const person = new Object({
+const person = {
   firstName: 'Elon',
   lastName: 'Musk',
-  age: 35
-}); 
-
-
-
-console.log(person);
+  greet: function greet() {
+    console.log('Hello World');
+  }
+}
+person.greet(); 
