3ce35b3b24b7aa137083de603c5c6a70c60d0ce2
Jackie Jr <jwysinger@gmail.com>
2023-11-29 14:48:46 -0600

    power function example

--- javascriptpractice.js
+++ javascriptpractice.js
@@ -1,11 +1,10 @@
-// JavaScript Object Prototype
+// Eloquent Javascript
 
-// Every function and object has its own property called prototype
-
-// A prototype itself is also another object. So, the prototype has its own prototyple. this creates a prototype chain
-
-const person = {
-  name: 'Elon'
+const power = function(base, exponent) { // power function with two parameters 2 , 10
+  let result = 1; // result is equal to 1
+  for(let count = 0; count < exponent; count++) { // count is less than exponent 10 increment count by one
+    result *= base; // result times base then set to product 1 * 2,  result = 2
+  }
+  return result;
 }
-
-console.log(person); 
+console.log(power(2, 10));
